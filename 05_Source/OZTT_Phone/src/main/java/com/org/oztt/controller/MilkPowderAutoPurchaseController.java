3569f1c97bbe73013492a83543c100f4bf19b322
linliuan <linliuanqdzb@163.com>
2017-03-16 20:57:20 +0800

    防止微信接口重复访问

--- 05_Source/OZTT_Phone/src/main/java/com/org/oztt/controller/MilkPowderAutoPurchaseController.java
+++ 05_Source/OZTT_Phone/src/main/java/com/org/oztt/controller/MilkPowderAutoPurchaseController.java
@@ -549,6 +549,9 @@ public class MilkPowderAutoPurchaseController extends BaseController {
                 if (!StringUtils.isEmpty(orderId)) {
                     logger.error("微信付款成功之后开始调用接口，订单号为：" + orderId);
                     TPowderOrder tPowderOrder = powderService.getTPowderOrderByOrderNo(orderId);
+                    if (tPowderOrder == null || CommonEnum.HandleFlag.PLACE_ORDER_SU.equals(tPowderOrder.getPaymentStatus())) {
+                        return mapReturn;
+                    }
                     // 优先更新付款方式
                     tPowderOrder.setPaymentMethod(CommonEnum.PaymentMethod.WE_CHAT.getCode());
                     powderService.updatePowderOrder(tPowderOrder);
