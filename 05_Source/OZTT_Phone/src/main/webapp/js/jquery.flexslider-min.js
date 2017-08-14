c25348c27a07a0f2a60ab0935dec71c5e4f1c109
linliuan <linliuanqdzb@163.com>
2017-08-14 22:27:40 +0800

    将冲突的方法删除

--- 05_Source/OZTT_Phone/src/main/webapp/js/jquery.flexslider-min.js
+++ 05_Source/OZTT_Phone/src/main/webapp/js/jquery.flexslider-min.js
@@ -499,29 +499,3 @@
 		}
 	}
 })(jQuery);
-//��ʱ��
-
-//function updateEndTime(){
-//	var date = new Date();
-//	var time = date.getTime();  //��ǰʱ���1970��1��1��֮��ĺ�
-//	$(".settime").each(function(i){
-//		var endDate =this.getAttribute("endTime"); //����ʱ���ַ���
-//		//ת��Ϊʱ����������
-//		var endDate1 = eval('new Date(' + endDate.replace(/\d+(?=-[^-]+$)/, function (a){return parseInt(a, 10) - 1; }).match(/\d+/g) + ')');
-//		//����ʱ�������
-//		var endTime = endDate1.getTime();
-//		//��ǰʱ��ͽ���ʱ��֮�������
-//		var lag = (endTime - time) / 1000;
-//		if(lag > 0)
-//		{
-//			var second = Math.floor(lag % 60);
-//			var minite = Math.floor((lag / 60) % 60);
-//			var hour = Math.floor((lag / 3600) % 24);
-//			var day = Math.floor((lag / 3600) / 24);
-//			$(this).html(day+"��"+hour+"Сʱ"+minite+"��"+second+"��");
-//		}
-//		else
-//			$(this).html("�ܱ�Ǹ���Ź���Ѿ���������");
-//	});
-//	setTimeout("updateEndTime()",1000);
-//}
